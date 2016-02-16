4e66a5b4f0e0ceaff10802792fec2699a67833cb
Summer Barton-Taylor <summerbartontaylor@gmail.com>
2016-02-16 09:41:42 -0600

    1.2.4

---
+++ script.js
@@ -0,0 +1,23 @@
+angular.module('myApp', [])
+    //create a Filter
+    .filter("reverseText", function () {
+        //    Defining the filter funtion
+        return function (input) {
+            var result = "";
+            input = input || "";
+            for (var i = 0; i < input.length; i++) {
+                result = input.charAt(i) + result;
+            }
+            return result;
+        };
+    })
+    .filter("titleCase", function () {
+        //defining the filter function
+        return function (input) {
+            input = (input === undefined || input === null) ? '' : input;
+            return input.toLowerCase().replace("/\b([a-b])/", function (change) {
+return
+            });
+
+        }
+    })
